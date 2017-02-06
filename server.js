eb6a1bad9d9ddd870809cf3fbf7c8299b839c811
Julien Fabre <julien.fabre@pole-emploi.fr>
2017-02-06 12:54:18 +0100

    chore(travisCI) : gulp test

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var restify = require('restify');
+"use strict";
+
+var restify = require("restify");
 var configServer = require("./config.json");
 var TransmitterMattermost = require("./src/transmitter-mattermost.js");
 var EncoderMattermost= require("./src/encoder-mattermost.js");
@@ -7,11 +9,11 @@ var transmitterMattermost = new TransmitterMattermost("./config.json");
 var encoderMattermost = new EncoderMattermost(configServer.sonar.url);
 
 var myServer = restify.createServer({
-  name: 'mattermost-sonar'
+  name: "mattermost-sonar"
 });
 
 myServer.use(restify.bodyParser());
-myServer.post('/mattermost-sonar', function (req, res, next) {
+myServer.post("/mattermost-sonar", function (req, res, next) {
   transmitterMattermost.pushToMattermost(encoderMattermost.encodeSonarMessage(req.body));
   res.send("OK");
   return next();
--- tests/src/encoder-mattermost-test.js
+++ tests/src/encoder-mattermost-test.js
@@ -1,17 +1,19 @@
-var chai = require('chai');
+"use strict";
+
+var chai = require("chai");
 var expect = chai.expect;
 var sonarExemple = require("./../resources/sonar.exemple.json");
-var EncoderMattermost = require('./../../src/encoder-mattermost');
+var EncoderMattermost = require("./../../src/encoder-mattermost");
 
 
-describe('EncoderMattermost', () => {
+describe("EncoderMattermost", () => {
     it('encodeSonarMessage() should return null if there\'s no message',()=> {
         var encoderMattermost = new EncoderMattermost();
         expect(encoderMattermost.encodeSonarMessage("")).to.be.null;
     });
-    it('encodeSonarMessage() should return an object with sonar.exemple.json',()=> {
+    it("encodeSonarMessage() should return an object with sonar.exemple.json",()=> {
         var encoderMattermost = new EncoderMattermost();
         var contenu = encoderMattermost.encodeSonarMessage(sonarExemple);
-        expect(contenu).not.be.null;
+        expect(contenu).to.be.not.null;
     });
 });
