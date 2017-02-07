e321a78226b3d03e4f35dba3793346ef62cff372
Julien Fabre <julien.fabre.33@gmail.com>
2017-02-07 23:37:50 +0100

    chore(jshint): add JsHint

--- tests/src/encoder-mattermost-test.js
+++ tests/src/encoder-mattermost-test.js
@@ -1,16 +1,17 @@
 var sonarExemple = require("./../resources/sonar.exemple.json");
-var EncoderMattermost = require('./../../src/encoder-mattermost');
+var TestEncoderMattermost = require('./../../src/encoder-mattermost');
 var chai = require('chai');
 var expect = chai.expect;
+chai.should();
 
 describe('EncoderMattermost', function() {
     it('encodeSonarMessage() should return null if there\'s no message',function() {
-        var encoderMattermost = new EncoderMattermost();
+        var encoderMattermost = new TestEncoderMattermost();
         expect(encoderMattermost.encodeSonarMessage("")).to.be.null;
     });
     it('encodeSonarMessage() should return an object with sonar.exemple.json',function() {
-        var encoderMattermost = new EncoderMattermost();
+        var encoderMattermost = new TestEncoderMattermost();
         var contenu = encoderMattermost.encodeSonarMessage(sonarExemple);
-        expect(contenu).not.be.null;
+        expect(contenu).should.be.not.null;
     });
 });
